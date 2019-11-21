680823d66872239f32cf90e4f9a8c09f07a1426e
gauravDublin <Irish2017#>
2019-11-22 00:01:08 +0100

    SKILLS-1919:  kafka producer gets the data from twitter

--- src/main/java/com/performance/sql/service/TwitterProducerService.java
+++ src/main/java/com/performance/sql/service/TwitterProducerService.java
@@ -1,6 +1,7 @@
 package com.performance.sql.service;
 
 import com.google.common.collect.Lists;
+import com.performance.sql.ApplicationCallBack;
 import com.performance.sql.config.TwitterConfig;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
@@ -11,12 +12,16 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -44,12 +49,26 @@ public class TwitterProducerService {
                 config.getToken(), config.getSecret());
         Client hosebirdClient = createClient(hosebirdHosts, hosebirdEndpoint, hosebirdAuth, msgQueue);
         hosebirdClient.connect();
-
+        KafkaProducer<String, String> producer = createKafkaProducer();
         // hosebirdClient
-        pollMessageFromQueue(hosebirdClient, msgQueue) ;
+        pollMessageFromQueue(hosebirdClient, msgQueue, producer) ;
     }
 
-    private void pollMessageFromQueue(Client hosebirdClient, BlockingQueue<String> msgQueue) {
+    private KafkaProducer<String, String> createKafkaProducer() {
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // KAFKA producer properties
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the  producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+        return producer;
+    }
+
+    private void pollMessageFromQueue(Client hosebirdClient, BlockingQueue<String> msgQueue, KafkaProducer<String, String> producer) {
         logger.info("Start producing");
         String msg = null;
         while (!hosebirdClient.isDone()) {
@@ -61,6 +80,14 @@ public class TwitterProducerService {
             }
             if(msg!=null) {
                 logger.info(msg);
+                producer.send(new ProducerRecord<>("twittertweets", null, msg), new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        if(e!=null) {
+                            logger.error("Something bad happened", e);
+                        }
+                    }
+                });
             }
         }
         logger.info("Stop producing");
